acdd8cafea219f1bfcb54a59222f7f27df4b4c11
theoroe3 <theo@jumpingrivers.com>
2018-12-20 16:33:36 +0000

    first attempt

--- R/jrPackage.R
+++ R/jrPackage.R
@@ -1,10 +1,10 @@
 #' R courses by Jumping Rivers
-#' 
-#' Functions and datasets used in for R 
+#'
+#' Functions and datasets used in for R
 #' courses run by Jumping Rivers. This package is used in
-#' the "Building a package" course".
-#' @name jrPackage-package
-#' @aliases jrPackage
+#' the "Advanced Programming / Building a package" course".
+#' @name jrAdvPackage-package
+#' @aliases jrAdvPackage
 #' @docType package
 #' @keywords package
 NULL
